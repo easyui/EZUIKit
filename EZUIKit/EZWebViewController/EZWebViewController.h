d77abb955fc701c62fafdf1f1f695e2975ce9627
cactus <522941939@qq.com>
2016-01-19 11:28:33 +0800

    add EZWebViewController

---
+++ EZUIKit/EZWebViewController/EZWebViewController.h
@@ -0,0 +1,13 @@
+//
+//  EZWebViewController.h
+//  EZUIKit
+//
+//  Created by yangjun zhu on 16/1/19.
+//  Copyright © 2016年 Cactus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EZWebViewController : UIViewController
+
+@end
