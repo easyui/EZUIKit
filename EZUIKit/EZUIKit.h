8fcc66d09366a01bf02b6dd28a41325b6481e1ad
cactus <522941939@qq.com>
2016-01-19 11:25:33 +0800

    create framework

---
+++ EZUIKit/EZUIKit.h
@@ -0,0 +1,19 @@
+//
+//  EZUIKit.h
+//  EZUIKit
+//
+//  Created by yangjun zhu on 16/1/19.
+//  Copyright © 2016年 Cactus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EZUIKit.
+FOUNDATION_EXPORT double EZUIKitVersionNumber;
+
+//! Project version string for EZUIKit.
+FOUNDATION_EXPORT const unsigned char EZUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EZUIKit/PublicHeader.h>
+
+
