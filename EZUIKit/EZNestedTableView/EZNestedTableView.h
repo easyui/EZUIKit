e88133cb5dd7ff551dd0b9fa2b984de7c5e012e5
easyui <gzhuyangjun@gmail.com>
2016-03-16 14:58:11 +0800

    add EZNestedTableViewSectionModelProtocol,EZNestedTableViewCellModelProtocol

--- EZUIKit/EZNestedTableView/EZNestedTableView.h
+++ EZUIKit/EZNestedTableView/EZNestedTableView.h
@@ -7,7 +7,26 @@
 //
 
 #import <UIKit/UIKit.h>
+NS_ASSUME_NONNULL_BEGIN
+
+
+@protocol EZNestedTableViewSectionModelProtocol <NSObject>
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, assign) BOOL isExpaned;
+@property (nonatomic, copy) NSArray *selectedItems;
+@property (nonatomic, copy) NSArray *items;//isSubTitle ,title,ischecked
+@end
+
+@protocol EZNestedTableViewCellModelProtocol <NSObject>
+@property (nonatomic, assign) BOOL isSubTitle;
+@property (nonatomic, copy) NSString *title;
+@property (nonatomic, assign) BOOL ischecked;
+@end
+
 
 @interface EZNestedTableView : UIView <UITableViewDataSource, UITableViewDelegate>
+@property (nonatomic, assign) IBInspectable BOOL isSingleExpanedOnly;
+@property (nonatomic, copy) NSArray<EZNestedTableViewSectionModelProtocol> * sectionModels;
 
 @end
+NS_ASSUME_NONNULL_END
