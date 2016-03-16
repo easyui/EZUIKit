5b0cc3d1d02dad70262bde4ddb41f3e6f205afd1
easyui <gzhuyangjun@gmail.com>
2016-03-16 12:01:58 +0800

    add EZNestedTableView h/m

---
+++ EZUIKit/EZNestedTableView/EZNestedTableView.m
@@ -0,0 +1,21 @@
+//
+//  EZNestedTableView.m
+//  EZUIKit
+//
+//  Created by yangjun zhu on 16/3/16.
+//  Copyright © 2016年 Cactus. All rights reserved.
+//
+
+#import "EZNestedTableView.h"
+
+@implementation EZNestedTableView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
